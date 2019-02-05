66047550e50af1e5c83ff4645ef8404ec22707e3
avcopan <avcopan@gmail.com>
2019-02-05 14:41:11 -0600

    Adds zmat-from-xyz-getter as a temp script

--- elstruct/reader/gaussian/__init__.py
+++ elstruct/reader/gaussian/__init__.py
@@ -10,7 +10,7 @@ from .surf import cartesian_hessian_reader
 from .surf import cartesian_gradient_reader
 from .surf import irc_geometries_reader
 from .surf import irc_cartesian_gradients_reader
-from .surf import irc_internal_gradients_reader
+# from .surf import irc_internal_gradients_reader
 from .surf import irc_cartesian_hessians_reader
 from .surf import irc_internal_hessians_reader
 from .surf import irc_reaction_path_reader
@@ -25,7 +25,7 @@ __all__ = [
     'cartesian_gradient_reader',
     'irc_geometries_reader',
     'irc_cartesian_gradients_reader',
-    'irc_internal_gradients_reader',
+    # 'irc_internal_gradients_reader',
     'irc_cartesian_hessians_reader',
     'irc_internal_hessians_reader',
     'irc_reaction_path_reader',
